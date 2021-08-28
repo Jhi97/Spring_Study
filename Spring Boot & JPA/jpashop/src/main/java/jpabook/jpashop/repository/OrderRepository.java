4fbf559052a9e5017e58cafda5c6f0080501e45d
jhi97 <jeon9771@naver.com>
2021-08-28 14:50:12 +0900

    Order and Item 도메인 개발

---
+++ Spring Boot & JPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        return em.createQuery(jpql, Order.class)
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+    }
+}
